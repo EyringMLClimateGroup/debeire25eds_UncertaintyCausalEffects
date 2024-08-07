9ebb9776c678381f92012e417d603f826bc569a6
Kevin Debeire <kevin.debeire@dlr.de>
2024-08-07 12:54:59 +0000

    Update sbatch_mpi_1seed.sh

--- sbatch_mpi_1seed.sh
+++ sbatch_mpi_1seed.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --ntasks-per-node=100 # number of cores per CPU /!\ NEED to be EQUAL to number of bootstrap realizations
+#SBATCH --ntasks-per-node=100 # number of cores per CPU /!\ This will be used as the number of bootstrap realizations
 #SBATCH --contiguous # Ensure that all of the cores are on the same Infiniband network, adjust if necessary
 #SBATCH --time=02:00:00 #adjust
 #SBATCH --partition=xxx ###partition name to adjust
